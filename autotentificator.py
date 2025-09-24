e42505041d4291021712c63bcbb9341094b830d9
collhoun <2904yr@mail.ru>
2025-09-24 09:59:35 +0300

    First commit. Create Flask app runner module named run.py. Add module autotentificator to check in users datas

---
+++ autotentificator.py
@@ -0,0 +1,14 @@
+
+class Autotentificator:
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return f'Class Autotentificator'
+
+    def find_user() -> bool:
+        pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ run.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/sign_in')
+def sign_in():
+    return render_template('sign_in.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

bfc255814fecde79cd817eaf0c7af875caff3a23
collhoun <2904yr@mail.ru>
2025-10-23 21:58:43 +0300

    Add autotentification module to allow user to sign in. Module should be updated later

--- autotentification.py
+++ autotentification.py
@@ -1,3 +1,7 @@
+from db import DateBase
+
+base = DateBase()
+
 
 class Autotentificator:
     def __init__(self):
@@ -6,8 +10,28 @@ class Autotentificator:
     def __repr__(self):
         return f'Class Autotentificator'
 
-    def find_user(self):
-        pass
+    def find_user(self, email: str, password: str) -> bool:  # type: ignore
+        """Находит пользователя в БД. Возвращает True если пользователь найден, False иначе
+
+        Args:
+            email (str): электронная почта пользователя
+            password (str): пароль пользователя
+
+        Raises:
+            ValueError: если пользователь ввел неверный пароль, то создается исключение и пользователь получает информацию о неверно 
+            введенных данных
+
+        Returns:
+            bool: 
+        """
+        rows = base.select('email, password', 'users', f'email == "{email}"')
+        if not rows:
+            return False
+        for em, psw in rows:
+            if em == email and psw == password:
+                return True
+            if em == email and psw != password:
+                raise ValueError("Неверно введены входные данные")
 
 
 if __name__ == '__main__':
--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, url_for, request, g, session, redirect
 from registration import Registartor
 from db import DateBase
+from autotentification import Autotentificator
 
+aut = Autotentificator()
 base = DateBase()
 rg = Registartor()
 app = Flask(__name__)
@@ -9,14 +11,23 @@ app.config['SECRET_KEY'] = 'MK-PIG'
 base.create_users_table()
 
 
-@app.route('/sign_in')
+@app.route('/sign_in', methods=['GET', 'POST'])
 def sign_in():
+    if request.method == 'POST':
+        try:
+            if aut.find_user(request.form['email'], request.form['password']):
+                return redirect(url_for('testor_form', msg='Вы авторизованы'))
+            else:
+                return render_template('sign_in.html', e="Неверный email или пароль")
+        except ValueError as e:
+            return render_template('sign_in.html', e=e)
+
     return render_template('sign_in.html')
 
 
-@app.route('/testor_form', methods=['POST', 'GET'])
-def testor_form():
-    return render_template('testor_form.html')
+@app.route('/testor_form/<msg>', methods=['POST', 'GET'])
+def testor_form(msg):
+    return render_template('testor_form.html', msg=msg)
 
 
 @app.route('/registration', methods=['GET', 'POST'])
@@ -25,7 +36,7 @@ def registration():
         try:
             if rg.check_correction_email(request.form['email']) and rg.find_user(request.form['email'], request.form['password']) == False:
                 if rg.reg(request.form['email'], request.form['password']):
-                    return redirect(url_for('testor_form'))
+                    return redirect(url_for('testor_form', msg='Вы зарегистрированы'))
         except ValueError as erorr:
             # добавить вывод ошибки в форму регистрации
             return render_template('registration.html', erorr=erorr)
